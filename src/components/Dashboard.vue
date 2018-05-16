2121a966650c2502dfc8b187a0bdcbddc80eadbf
Mondeer <mondiakering@gmail.com>
2018-05-16 15:49:42 +0300

    edited dashboad

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -52,22 +52,27 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-6">
-        <h1>Clockin</h1>
+      <div class="col-md-10">
+        <button @click="exportRecord">Download Record</button>
+        <h1>Clock Records</h1>
         <table class="table table-striped">
         <thead>
           <tr>
-            <th>Clock Time</th>
-            <th>User ID</th>
+            <th>PF No.</th>
+            <th>Full Name</th>
+            <th>Clockin Time</th>
+            <th>Clockout Time</th>
+            <th>Time In</th>
           </tr>
         </thead>
         <tbody>
-          <ol>
-            <li v-for="clockin in clockins" :key='clockin'>
-              <td>{{clockin.clocktime}}</td>
-              <td>{{clockin.userID}}</td>
-              <td>{{clockin.clockinID}}</td>
-            </li>
+            <tr v-for="record in records" :key='record'>
+              <td>{{record.emppf_no}}</td>
+              <td>{{record.empName}}</td>
+              <td>{{record.clockin_time}}</td>
+              <td>{{record.clockout_time}}</td>
+              <td>{{record.time_in}}</td>
+            </tr>
           </ol>
         </tbody>
       </table>
@@ -84,17 +89,32 @@ export default{
     return {
       clockouts:[],
       clockins:[],
-      employees:[]
+      employees:[],
+      records:[]
     }
   },
   methods: {
-
+    exportRecord(){
+      var vm = this
+      var columns = [
+        {title: "Emppf_no", dataKey: "emppf_no"},
+        {title: "Employee Name", dataKey: "name"},
+        {title: "Clockin Time", dataKey: "clockin_time"},
+        {title: "Clockout Time", dataKey: "clockout_time"},
+        {title: "Time in", dataKey: "time_in"}
+      ];
+      var doc = new jsPDF('p', 'pt');
+      doc.text("Employee Clock Data", 10, 12);
+      doc.autoTable(columns, vm.records);
+      doc.save('records.pdf');
+    }
   },
   created() {
     firebase.database().ref('RevolutionBios').on('value', (snapshot)=>{
       this.employees = snapshot.child('employees').val();
       this.clockins = snapshot.child('clockins').val();
       this.clockouts = snapshot.child('clockouts').val();
+      this.records = snapshot.child('records').val();
     });
   }
 }
